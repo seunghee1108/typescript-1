21965498ded39047b166c78984637f5be929ed84
승희 <bangseunghee1108@gmail.com>
2023-12-05 17:01:47 +0900

    Union Type-ts

--- test6.ts
+++ test6.ts
@@ -2,4 +2,12 @@ type Status = "success" | "error";
 
 const result : Status = "success";
 
-console.log(result);
+console.log(result); // success
+
+// -----
+
+let age : number;
+
+age = 10;
+
+console.log(age); // 10
