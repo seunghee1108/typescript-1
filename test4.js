a6654f8e3794a522c88f30449a457630cc191e33
승희 <bangseunghee1108@gmail.com>
2023-12-05 22:24:05 +0900

    array, tuple-js

--- test4.js
+++ test4.js
@@ -2,3 +2,13 @@ var numbers = [1, 2, 3];
 var test = ["Bang", 28];
 console.log(numbers);
 console.log(test);
+var numArr = [1, 2, 3];
+var strArr = ['1', '2', '3'];
+console.log(numArr);
+console.log(strArr);
+var person;
+// 초기화
+person = ["Bang", 28];
+// 잘못된 초기화 
+// person = [10, "Bang"];
+console.log(person);
