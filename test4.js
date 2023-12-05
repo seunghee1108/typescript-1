3af91ba3d263eed74ff96488628fc56116943361
승희 <bangseunghee1108@gmail.com>
2023-12-05 16:54:49 +0900

    변수명 수정

--- test4.js
+++ test4.js
@@ -1,4 +1,4 @@
 var numbers = [1, 2, 3];
-var tuple = ["Bang", 28];
-console.log(numbers); // [1, 2, 3]
-console.log(tuple); // ['Bang', 28]
+var test = ["Bang", 28];
+console.log(numbers);
+console.log(test);
