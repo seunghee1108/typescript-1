f16ea0fec902fc7f67cd28e0e73ae58d11c6bbf6
승희 <bangseunghee1108@gmail.com>
2023-12-05 17:00:26 +0900

    type-예제 추가-js

--- test5.js
+++ test5.js
@@ -1,2 +1,7 @@
-var point = { x: 5, y: 3 };
-console.log(point); // { x: 5, y: 3 }
+// type Point = {
+//   x : number;
+//   y : number;
+// };
+var point = { x: "하이", y: { name: "Bang" }, z: 3 };
+console.log(point);
+// { x: '하이', y: { name: 'Bang' }, z: 3 }
