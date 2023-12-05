38f5de93b44766ba636b4776e0cf475935dcbad3
승희 <bangseunghee1108@gmail.com>
2023-12-05 22:23:46 +0900

    array, tuple

--- test4.ts
+++ test4.ts
@@ -1,5 +1,21 @@
-let numbers : number[] = [1, 2, 3];
+ let numbers : number[] = [1, 2, 3];
 let test : [string, number] = ["Bang", 28];
 
 console.log(numbers);
 console.log(test);
+
+
+let numArr: number[] = [1, 2, 3];
+let strArr: string[] = ['1', '2', '3'];
+
+console.log(numArr);
+console.log(strArr);
+
+let person : [string, number]
+
+// 초기화
+person = ["Bang", 28];
+// 잘못된 초기화 
+// person = [10, "Bang"];
+
+console.log(person);
