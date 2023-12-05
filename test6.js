dafaed3a78848143a73a95b05d49855fcaca00da
승희 <bangseunghee1108@gmail.com>
2023-12-05 22:27:29 +0900

    Union Type-js

--- test6.js
+++ test6.js
@@ -1,2 +1,10 @@
 var result = "success";
-console.log(result);
+console.log(result); // success
+// -----
+var age;
+age = 10;
+console.log(age); // 10
+// let color : Color;
+// color = "Black";
+var color = "Black";
+console.log(color);
