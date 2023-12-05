de21febfb0ffb38deb95a072cd4c647cb2f4fc44
승희 <bangseunghee1108@gmail.com>
2023-12-05 16:59:46 +0900

    type-예제 추가-ts

--- test5.ts
+++ test5.ts
@@ -1,8 +1,19 @@
+// type Point = {
+//   x : number;
+//   y : number;
+// };
+
+// const point : Point = { x : 5, y : 3 };
+
+// console.log(point);
+
 type Point = {
-  x : number;
-  y : number;
+  x : string;
+  y : object;
+  z : number;
 };
 
-const point : Point = { x : 5, y : 3 };
+const point : Point = { x : "하이", y : { name : "Bang" } , z : 3};
 
 console.log(point);
+// { x: '하이', y: { name: 'Bang' }, z: 3 }s
